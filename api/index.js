4ed83a219ccb19ecd19f564916d77fefa25b5eed
bz <88026162+bz-hashtag-0780@users.noreply.github.com>
2023-08-14 18:50:40 +0200

    initial discord bot setup

--- api/index.js
+++ api/index.js
@@ -1,25 +1,33 @@
-const { Client, Intents } = require('discord.js');
-require('dotenv').config();
+import { Client, GatewayIntentBits } from 'discord.js';
+import dotenv from 'dotenv';
 
-const myIntents = new Intents([
-	Intents.FLAGS.GUILDS,
-	Intents.FLAGS.GUILD_MESSAGES,
-]);
+dotenv.config();
 
-const client = new Client({ intents: myIntents });
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+	],
+});
 
-client.once('ready', () => {
+client.on('ready', () => {
 	console.log('Bot is online!');
 });
 
 client.on('messageCreate', (message) => {
 	if (message.author.id !== '854000040887582730') return;
+	if (message.channel.id !== '897100198738796555') return;
 
 	// Check if the message content matches the command
 	if (message.content === '!raid') {
 		console.log('Raid command received!');
 		message.reply('Raid command recognized!');
 	}
+
+	// console.log(
+	// 	`Message received: Content: "${message.content}", Author: "${message.author.tag}", Channel: "${message.channel.name}"`
+	// );
 });
 
 client.login(process.env.DISCORD_BOT_TOKEN);
