f4ae58018643a0453c2e306be4e7e42c13dece8a
bz <88026162+bz-hashtag-0780@users.noreply.github.com>
2023-08-15 10:32:31 +0200

    re-org and add express

---
+++ api/commands/raid.js
@@ -0,0 +1,14 @@
+export const raid = {
+	name: '!raid',
+	description: 'Handles the !raid command',
+	execute: async (message) => {
+		// Your raid logic here
+		if (false) {
+			await message.reply('Response for condition 1');
+		} else if (false) {
+			await message.reply('Response for condition 2');
+		} else {
+			await message.reply('Default response');
+		}
+	},
+};
---
+++ api/server.js
@@ -0,0 +1,53 @@
+import { Client, GatewayIntentBits } from 'discord.js';
+import dotenv from 'dotenv';
+import { raid } from './commands/raid.js'; // Import the raid command
+import express from 'express';
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.get('/', (req, res) => {
+	res.send('Discord bot is online!');
+});
+
+app.listen(PORT, async () => {
+	console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+	],
+});
+
+const commands = new Map(); // Create a Map to store the commands
+commands.set(raid.name, raid); // Register the raid command
+
+client.on('ready', () => {
+	console.log('Bot is online!');
+});
+
+client.on('messageCreate', async (message) => {
+	if (message.author.id !== '854000040887582730') return;
+	if (message.channel.id !== '897100198738796555') return;
+
+	const args = message.content.trim().split(/ +/g);
+	const commandName = args.shift().toLowerCase(); // Extracting the command name
+
+	// Check if the command exists in our command Map
+	if (commands.has(commandName)) {
+		try {
+			commands.get(commandName).execute(message, args);
+		} catch (error) {
+			console.error('Error executing command:', error);
+			await message.reply(
+				'There was an error trying to execute that command!'
+			);
+		}
+	}
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN);
